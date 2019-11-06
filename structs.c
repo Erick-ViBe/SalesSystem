30d3788aa37ee7205d7e760a54e495c46152b4ea
Erick Vicencio Benitez <erickvb12@gmail.com>
2019-11-05 18:40:06 -0600

    Hice los struct bro, no supe como poner la fecha y hora

---
+++ structs.c
@@ -0,0 +1,30 @@
+##include "stdio.h"
+##include "stdlib.h"
+
+struct Producto {
+
+   int codigo;
+   char nombre[];
+   float precio;
+};
+
+struct Cliente {
+
+   char nombre[];
+   int id_targeta;
+};
+
+struct ticket {
+
+   struct Cliente;
+   struct Lista;
+   float total_compra;
+   int-o no se cual sea- fecha;
+   int -o no se cual sea hora;
+};
+
+int main(int argc, char const *argv[]) {
+
+
+   return 0;
+}
