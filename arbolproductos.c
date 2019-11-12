e4e9e71f2809f3ca66a4bb65942179608f9c04de
Erick Vicencio Benitez <erickvb12@gmail.com>
2019-11-12 11:25:53 -0600

    Codigo de insertar en arbol, no terminado

--- arbolproductos.c
+++ arbolproductos.c
@@ -12,22 +12,46 @@ struct Nodo {
 
    struct Producto producto_almacenado;
    struct Nodo *hijo_derecho;
-   struct Nodo *hijo_izquierdo;
+   struct Nodo *hijo_izquierdo;Quedo implementada la funcion de buscar producto, sin haber hecho prueba
 };
 
-struct Producto buscar(int codigo_a_buscar, struct Nodo raiz_arbol_productos) {
+struct Producto buscar(int codigo_a_buscar, struct Nodo *raiz_arbol_productos) {
 
    if (raiz_arbol_productos == null) {
 
       return null;
-   } else if (raiz_arbol_productos.producto_almacenado.codigo == codigo_a_buscar) {
+   } else if (raiz_arbol_productos->producto_almacenado.codigo == codigo_a_buscar) {
 
-      return raiz_arbol_productos.producto_almacenado;
-   } else if (codigo_a_buscar < raiz_arbol_productos.producto_almacenado.codigo) {
+      return raiz_arbol_productos->producto_almacenado;
+   } else if (codigo_a_buscar < raiz_arbol_productos->producto_almacenado.codigo) {
 
-      return buscar(codigo_a_buscar, raiz_arbol_productos.hijo_izquierdo);
+      return buscar(codigo_a_buscar, raiz_arbol_productos->hijo_izquierdo);
    } else {
 
-      return codigo_a_buscar(codigo_a_buscar, raiz_arbol_productos.hijo_derecho);
+      return codigo_a_buscar(codigo_a_buscar, raiz_arbol_productos->hijo_derecho);
+   }
+}
+
+struct Nodo* crearNodo(struct Producto datosNodo){
+
+}
+
+//No terminado
+void insertar(struct Producto producto_a_insertar, struct Nodo *raiz_arbol_productos) {
+
+   struct Nodo* nodoCreado;
+   *nodoCreado = crearNodo(producto_a_insertar);
+
+   struct Nodo* auxiliar;
+
+   if (raiz_arbol_productos == null) {
+
+      return null;
+   } else if (nodoCreado->producto_almacenado.codigo < raiz_arbol_productos->producto_almacenado.codigo) {
+
+      return buscar(codigo_a_buscar, raiz_arbol_productos->hijo_izquierdo);
+   } else {
+
+      return codigo_a_buscar(codigo_a_buscar, raiz_arbol_productos->hijo_derecho);
    }
 }
