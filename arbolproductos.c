9065f9e5569a19a68723cacbead5f21a422bf324
Erick Vicencio Benitez <erickvb12@gmail.com>
2019-11-11 18:04:07 -0600

    Quedo implementada la funcion de buscar producto, sin haber hecho prueba

--- arbolproductos.c
+++ arbolproductos.c
@@ -11,6 +11,23 @@ struct Producto {
 struct Nodo {
 
    struct Producto producto_almacenado;
-   struct Nodo *nododerecho;
-   struct Nodo *nodoizquierdo;
+   struct Nodo *hijo_derecho;
+   struct Nodo *hijo_izquierdo;
 };
+
+struct Producto buscar(int codigo_a_buscar, struct Nodo raiz_arbol_productos) {
+
+   if (raiz_arbol_productos == null) {
+
+      return null;
+   } else if (raiz_arbol_productos.producto_almacenado.codigo == codigo_a_buscar) {
+
+      return raiz_arbol_productos;
+   } else if (codigo_a_buscar < raiz_arbol_productos.producto_almacenado.codigo) {
+
+      return buscar(codigo_a_buscar, raiz_arbol_productos.hijo_izquierdo);
+   } else {
+
+      return codigo_a_buscar(codigo_a_buscar, raiz_arbol_productos.hijo_derecho);
+   }
+}
