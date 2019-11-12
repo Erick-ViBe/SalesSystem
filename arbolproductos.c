0c3dc5905c3b3a4084a13e590481596a575dec19
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-12 11:39:21 -0600

    .

--- arbolproductos.c
+++ arbolproductos.c
@@ -9,7 +9,6 @@ struct Producto {
 };
 
 struct Nodo {
-
    struct Producto producto_almacenado;
    struct Nodo *hijo_derecho;
    struct Nodo *hijo_izquierdo;
@@ -18,17 +17,17 @@ struct Nodo {
 struct Producto buscar(int codigo_a_buscar, struct Nodo *raiz_arbol_productos) {
 
    if (raiz_arbol_productos == null) {
-
       return null;
+
    } else if (raiz_arbol_productos->producto_almacenado.codigo == codigo_a_buscar) {
-
       return raiz_arbol_productos->producto_almacenado;
+
    } else if (codigo_a_buscar < raiz_arbol_productos->producto_almacenado.codigo) {
-
       return buscar(codigo_a_buscar, raiz_arbol_productos->hijo_izquierdo);
-   } else {
 
+   } else {
       return codigo_a_buscar(codigo_a_buscar, raiz_arbol_productos->hijo_derecho);
+      
    }
 }
 
