a15be05aad69fb2f44b76528a9492ec1d17605ce
Erick-ViBe <erickvb12@gmail.com>
2019-11-22 17:04:59 -0600

    Creacion del .h y .c

---
+++ Proyecto/supermercado.c
@@ -0,0 +1,217 @@
+#include "supermercado.h"
+
+struct Producto
+leerProducto(){
+
+   struct Producto nuevo;
+   float precioProducto;
+   char nombreProducto[25];
+
+   scanf("Precio del Producto: %f\n", &precioProducto);
+   scanf("Nombre del Producto: %[^\n]",nombreProducto);
+
+   nuevo.precio = precioProducto;
+   strcpy(nuevo.nombre, nombreProducto);
+   nuevo.codigo = obtenerCodigo(nombreProducto);
+
+   return nuevo;
+}
+
+struct Cliente
+leerCliente(){
+
+   struct Cliente nuevo;
+   char nombreCliente[30];
+   int idTarjeta;
+
+   scanf("Nombre del Cliente: %[^\n]",nombreCliente);
+   scanf("Id de la tajeta: %d", idTarjeta);
+
+   strcpy(nuevo.nombre, nombreCliente);
+   nuevo.id_tarjeta = idTarjeta;
+
+   return nuevo;
+}
+
+struct Arbol*
+insertar(struct Arbol *raizArbol, struct Producto productoInsertar){
+
+   struct Arbol *nuevo = NULL;
+
+   if (raizArbol == NULL) {
+
+      nuevo = malloc(sizeof(struct Arbol));
+      if (nuevo == NULL) {
+         return NULL;
+      }
+      nuevo->productoAlmacenado = productoInsertar;
+      nuevo->fe = 0;
+      nuevo->hijoDerecho = NULL;
+      nuevo->hijoIzquierdo = NULL;
+      return nuevo;
+   }
+
+   if (raizArbol->productoAlmacenado.codigo == productoInsertar.codigo) {
+
+      return raizArbol;
+   }
+
+   if (productoInsertar.codigo > raizArbol->productoAlmacenado.codigo) {
+
+      raizArbol->hijoDerecho = insertar(raizArbol->hijoDerecho, productoInsertar);
+   } else {
+
+      raizArbol->hijoIzquierdo = insertar(raizArbol->hijoIzquierdo, productoInsertar);
+   }
+
+   raizArbol->fe = calcularFE(raizArbol);
+   if ((raizArbol->fe == -2) && (raizArbol->hijoIzquierdo->fe == -1)) {
+			raizArbol = rotacionDerecha(raizArbol);
+			raizArbol->fe = calcularFE(raizArbol);
+			raizArbol->hijoDerecho->fe = calcularFE(raizArbol->hijoDerecho);
+			raizArbol->hijoIzquierdo->fe = calcularFE(raizArbol->hijoIzquierdo);
+		}
+
+		if ((raizArbol->fe == 2) && (raizArbol->hijoDerecho->fe == 1)) {
+			raizArbol = rotacionIzquierda(raizArbol);
+			raizArbol->fe = calcularFE(raizArbol);
+			raizArbol->hijoDerecho->fe = calcularFE(raizArbol->hijoDerecho);
+			raizArbol->hijoIzquierdo->fe = calcularFE(raizArbol->hijoIzquierdo);
+		}
+
+		if ((raizArbol->fe > 1) && (raizArbol->hijoDerecho->fe < 0)) {
+			raizArbol->hijoDerecho = rotacionDerecha(raizArbol->hijoDerecho);
+			raizArbol = rotacionIzquierda(raizArbol);
+			raizArbol->fe = calcularFE(raizArbol);
+			raizArbol->hijoDerecho->fe = calcularFE(raizArbol->hijoDerecho);
+			raizArbol->hijoIzquierdo->fe = calcularFE(raizArbol->hijoIzquierdo);
+		}
+
+		if ((raizArbol->fe < -1) && (raizArbol->hijoIzquierdo->fe > 0)) {
+			raizArbol->hijoIzquierdo = rotacionIzquierda(raizArbol->hijoIzquierdo);
+			raizArbol = rotacionDerecha(raizArbol);
+			raizArbol->fe = calcularFE(raizArbol);
+			raizArbol->hijoDerecho->fe = calcularFE(raizArbol->hijoDerecho);
+			raizArbol->hijoIzquierdo->fe = calcularFE(raizArbol->hijoIzquierdo);
+		}
+
+   return raizArbol;
+}
+
+struct Arbol*
+rotacionDerecha(struct Arbol *raizDesbalanceada) {
+
+	struct Arbol *aux = raizDesbalanceada;
+		struct Arbol *aux2 = NULL;
+	raizDesbalanceada = raizDesbalanceada->hijoIzquierdo;
+
+	if (raizDesbalanceada->hijoDerecho != NULL) {
+
+		aux2 = raizDesbalanceada->hijoDerecho;
+	}
+
+	raizDesbalanceada->hijoDerecho = aux;
+
+	raizDesbalanceada->hijoDerecho->hijoIzquierdo = aux2;
+
+	return raizDesbalanceada;
+
+}
+
+struct Arbol*
+rotacionIzquierda(struct Arbol *raizDesbalanceada) {
+
+	struct Arbol *aux = raizDesbalanceada;
+		struct Arbol *aux2 = NULL;
+	raizDesbalanceada = raizDesbalanceada->hijoDerecho;
+
+	if (raizDesbalanceada->hijoIzquierdo != NULL) {
+
+		aux2 = raizDesbalanceada->hijoIzquierdo;
+	}
+
+	raizDesbalanceada->hijoIzquierdo = aux;
+
+	raizDesbalanceada->hijoIzquierdo->hijoDerecho = aux2;
+
+	return raizDesbalanceada;
+
+}
+
+int
+calcularAltura(struct Arbol *raiz){
+
+	int alturaDerecha = 0;
+	int alturaIzquierda = 0;
+
+	if (raiz == NULL) {
+		return -1;
+	}
+
+	alturaIzquierda = calcularAltura(raiz->hijoIzquierdo);
+	alturaDerecha = calcularAltura(raiz->hijoDerecho);
+
+	if (alturaIzquierda > alturaDerecha) {
+		return alturaIzquierda+1;
+	}
+	return alturaDerecha+1;
+}
+
+int
+calcularFE(struct Arbol *raiz){
+
+	int alturaDerecha = 0;
+	int alturaIzquierda = 0;
+
+	if (raiz == NULL) {
+		return 0;
+	}
+
+	alturaIzquierda = calcularAltura(raiz->hijoIzquierdo);
+	alturaDerecha = calcularAltura(raiz->hijoDerecho);
+
+	return alturaDerecha - alturaIzquierda;
+}
+
+struct Producto
+buscar(struct Arbol *raiz, int codigoBuscar){
+
+	if (raiz == NULL) {
+      struct Producto noExiste;
+      noExiste.codigo = 0;
+      noExiste.precio = 0;
+      strcpy(noExiste.nombre, "");
+		return noExiste;
+	} else if (raiz->productoAlmacenado.codigo == codigoBuscar) {
+
+		return raiz->productoAlmacenado;
+	}	else if (codigoBuscar < raiz->productoAlmacenado.codigo) {
+
+		return buscar(raiz->hijoIzquierdo, codigoBuscar);
+	} else{
+
+		return buscar(raiz->hijoDerecho, codigoBuscar);
+	}
+}
+
+int
+modificar(struct Arbol *raiz, int codigoBuscar){
+
+	if (raiz == NULL) {
+
+      return 0;
+	} else if (raiz->productoAlmacenado.codigo == codigoBuscar) {
+
+		printf("%f\n", raiz->productoAlmacenado.precio);
+      float nuevoPrecio;
+      scanf("Nuevo Precio:%f", &nuevoPrecio);
+      raiz->productoAlmacenado.precio = nuevoPrecio;
+      return 1;
+	}	else if (codigoBuscar < raiz->productoAlmacenado.codigo) {
+
+		return modificar(raiz->hijoIzquierdo, codigoBuscar);
+	} else{
+
+		return modificar(raiz->hijoDerecho, codigoBuscar);
+	}
+}
---
+++ Proyecto/supermercado.h
@@ -0,0 +1,43 @@
+#ifndef __SUPERMERCADO_H__
+#define __SUPERMERCADO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ctype.h"
+#include "stdlib.h"
+
+struct Cliente {
+
+   char nombre[30];
+   int id_tarjeta;
+};
+
+struct Producto {
+
+   int codigo;
+   char nombre[25];
+   float precio;
+};
+
+struct Arbol {
+
+   struct Producto productoAlmacenado;
+   struct Arbol *hijoDerecho;
+   struct Arbol *hijoIzquierdo;
+   int fe;
+};
+
+struct Producto leerProducto();
+struct Cliente leerCliente();
+struct Arbol* insertar(struct Arbol *raizArbol, struct Producto productoInsertar);
+struct Arbol* rotacionDerecha(struct Arbol *raizDesbalanceada);
+struct Arbol* rotacionIzquierda(struct Arbol *raizDesbalanceada);
+int calcularAltura(struct Arbol *raiz);
+int calcularFE(struct Arbol *raiz);
+struct Producto buscar(struct Arbol *raiz, int codigoBuscar);
+int modificar(struct Arbol *raiz, int codigoBuscar);
+char enteroacaracter(int numero);
+int obtenerCodigo(char *palabra);
+
+#endif
