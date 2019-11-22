f6014ec5a824a3457d8c208601a8afd6c3bcd966
Erick-ViBe <erickvb12@gmail.com>
2019-11-22 17:48:43 -0600

    Act

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -6,7 +6,9 @@ int main(void) {
 
    raiz = insertar(raiz, leerProducto());
 
-   printf("Nombre %s\n", raiz->productoAlmacenado.nombre);
+   printf("Nombre: %s\n", raiz->productoAlmacenado.nombre);
+   printf("Codigo: %d\n", raiz->productoAlmacenado.codigo);
+   printf("Precio %f\n", raiz->productoAlmacenado.precios);
 
    return 0;
 }
