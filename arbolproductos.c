0a68453495fd8931db343dd7966dc077a8c00c14
Erick-ViBe <erickvb12@gmail.com>
2019-11-22 16:23:28 -0600

    Actualizacion

--- arbolproductos.c
+++ arbolproductos.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ctype.h"
+#include "stdlib.h"
 
 struct Producto {
 
@@ -17,7 +19,6 @@ struct Arbol {
    int fe;
 };
 
-int generarCodigo();
 struct Producto leerProducto();
 struct Arbol* insertar();
 struct Arbol* rotacionDerecha();
@@ -25,114 +26,177 @@ struct Arbol* rotacionIzquierda();
 int calcularAltura();
 int calcularFE();
 struct Producto buscar();
+int modificar();
+char enteroacaracter();
+int obtenerCodigo();
 
 int main(void) {
 
-   struct Arbol *raiz = NULL;
+   // struct Arbol *raiz = NULL;
+   //
+   // struct Producto nuevo;
+   // nuevo.codigo = 13;
+   // nuevo.precio = 5.69;
+   // strcpy(nuevo.nombre, "agua");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 2;
+   // nuevo.precio = 8.17;
+   // strcpy(nuevo.nombre, "mole");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 4;
+   // nuevo.precio = 25.29;
+   // strcpy(nuevo.nombre, "sal");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 5;
+   // nuevo.precio = 5.69;
+   // strcpy(nuevo.nombre, "azucar");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 36;
+   // nuevo.precio = 8.17;
+   // strcpy(nuevo.nombre, "cloro");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 11;
+   // nuevo.precio = 25.29;
+   // strcpy(nuevo.nombre, "jabon");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 42;
+   // nuevo.precio = 5.69;
+   // strcpy(nuevo.nombre, "atun");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 0;
+   // nuevo.precio = 8.17;
+   // strcpy(nuevo.nombre, "queso");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // nuevo.codigo = 12;
+   // nuevo.precio = 25.29;
+   // strcpy(nuevo.nombre, "cafe");
+	// raiz = insertar(raiz, nuevo);
+   //
+   // printf("%d - (%f) - (%s)\n", raiz->productoAlmacenado.codigo , raiz->productoAlmacenado.precio, raiz->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoDerecho->productoAlmacenado.codigo , raiz->hijoDerecho->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoDerecho->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoIzquierdo->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.codigo, raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.precio, raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.nombre);
+	// printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.nombre);
+   //
+   // nuevo = buscar(raiz, 11);
+   //
+   // printf("%d - (%f) - (%s)\n", nuevo.codigo , nuevo.precio, nuevo.nombre);
 
-   struct Producto nuevo;
-   nuevo.codigo = 13;
-   nuevo.precio = 5.69;
-   strcpy(nuevo.nombre, "agua");
-	raiz = insertar(raiz, nuevo);
+   // modificar(raiz, 12);
 
-   nuevo.codigo = 2;
-   nuevo.precio = 8.17;
-   strcpy(nuevo.nombre, "mole");
-	raiz = insertar(raiz, nuevo);
+   int desicionesMenu;
 
-   nuevo.codigo = 4;
-   nuevo.precio = 25.29;
-   strcpy(nuevo.nombre, "sal");
-	raiz = insertar(raiz, nuevo);
+   printf("1-Cliente\n");
+   printf("2-Administrador\n");
+   scanf("%d", &desicionesMenu);
 
-   nuevo.codigo = 5;
-   nuevo.precio = 5.69;
-   strcpy(nuevo.nombre, "azucar");
-	raiz = insertar(raiz, nuevo);
+   switch (desicionesMenu) {
+      case 1:
+         printf("1-Comprar\n");
+         scanf("%d", &desicionesMenu);
+         switch (desicionesMenu) {
+            case 1:
 
-   nuevo.codigo = 36;
-   nuevo.precio = 8.17;
-   strcpy(nuevo.nombre, "cloro");
-	raiz = insertar(raiz, nuevo);
+            break;
+            default:
+               printf("nel\n");
+         }
+      break;
+      case 2:
+         printf("1-Registrar Producto\n");
+         printf("2-Modificar Producto\n");
+         scanf("%d", &desicionesMenu);
+         switch (desicionesMenu) {
+            case 1:
 
-   nuevo.codigo = 11;
-   nuevo.precio = 25.29;
-   strcpy(nuevo.nombre, "jabon");
-	raiz = insertar(raiz, nuevo);
+               break;
+            case 2:
 
-   nuevo.codigo = 42;
-   nuevo.precio = 5.69;
-   strcpy(nuevo.nombre, "atun");
-	raiz = insertar(raiz, nuevo);
-
-   nuevo.codigo = 0;
-   nuevo.precio = 8.17;
-   strcpy(nuevo.nombre, "queso");
-	raiz = insertar(raiz, nuevo);
-
-   nuevo.codigo = 12;
-   nuevo.precio = 25.29;
-   strcpy(nuevo.nombre, "cafe");
-	raiz = insertar(raiz, nuevo);
-
-   printf("%d - (%f) - (%s)\n", raiz->productoAlmacenado.codigo , raiz->productoAlmacenado.precio, raiz->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoDerecho->productoAlmacenado.codigo , raiz->hijoDerecho->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoDerecho->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoDerecho->hijoDerecho->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoDerecho->hijoIzquierdo->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.codigo, raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.precio, raiz->hijoDerecho->hijoIzquierdo->hijoDerecho->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoIzquierdo->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.codigo, raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.precio, raiz->hijoIzquierdo->hijoDerecho->productoAlmacenado.nombre);
-	printf("%d - (%f) - (%s)\n", raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.codigo, raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.precio, raiz->hijoIzquierdo->hijoIzquierdo->productoAlmacenado.nombre);
-
-   nuevo = buscar(raiz, 11);
-
-   printf("%d - (%f) - (%s)\n", nuevo.codigo , nuevo.precio, nuevo.nombre);
+               break;
+            default:
+               printf("nel\n");
+         }
+      break;
+      default:
+         printf("nel\n");
+   }
 
    return 0;
 }
 
-// int generarCodigo(char nombreProducto[25]){
-//
-//    if (nombreProducto = "agua") {
-//       return 13;
-//    } else if (nombreProducto = "mole") {
-//       return 2;
-//    } else if (nombreProducto = "sal") {
-//       return 4;
-//    } else if (nombreProducto = "azucar") {
-//       return 5;
-//    } else if (nombreProducto = "cloro") {
-//       return 36;
-//    } else if (nombreProducto = "jabon") {
-//       return 11;
-//    } else if (nombreProducto = "atun") {
-//       return 42;
-//    } else if (nombreProducto = "queso") {
-//       return 0;
-//    } else if (nombreProducto = "cafe") {
-//       return 12;
-//    }
-//
-// }
-//
-// struct Producto
-// leerProducto(){
-//
-//    struct Producto nuevo;
-//    float precioProducto;
-//    char nombreProducto[25];
-//
-//    scanf("Precio del Producto: %f\n", &precioProducto);
-//    scanf("Nombre del Producto: %s\n", &nombreProducto);
-//
-//    nuevo.precio = precioProducto;
-//    nuevo.nombre = nombreProducto;
-//    nuevo.codigo = generarCodigo(nombreProducto);
-//
-//    return nuevo;
-// }
+char enteroacaracter(int numero){
+  return numero + '0';
+}
+
+int obtenerCodigo(char *palabra){
+
+  int acumulado = 0;
+  char parte2[8];
+
+  for(int i = 0; i < strlen(palabra); ++i){
+      if(isupper(palabra[i])){
+        palabra[i] = tolower(palabra[i]);
+      }
+  }
+
+  for(int i = 0; i < strlen(palabra); ++i){
+    acumulado += palabra[i];
+  }
+
+  	char *parte1 = malloc(4 * sizeof(char));
+  	sprintf(parte1, "%d", acumulado);
+
+  int valor;
+  char convertido;
+
+  int i = 0;
+  for(i = 0; i < strlen(palabra) && i < 6; i++){
+
+    valor = palabra[i] % 9;
+    if(valor == 0){
+      valor = 9;
+    }
+    convertido = enteroacaracter(valor);
+    parte2[i] = convertido;
+  }
+
+  parte2[i] = '\0';
+
+  strcat(parte1, parte2);
+
+  int num = atoi(parte1);
+
+  return num;
+}
+
+struct Producto
+leerProducto(){
+
+   struct Producto nuevo;
+   float precioProducto;
+   char nombreProducto[25];
+
+   scanf("Precio del Producto: %f\n", &precioProducto);
+   scanf("Nombre del Producto: %[^\n]",nombreProducto);
+
+   nuevo.precio = precioProducto;
+   strcpy(nuevo.nombre, nombreProducto);
+   nuevo.codigo = obtenerCodigo(nombreProducto);
+
+   return nuevo;
+}
 
 struct Arbol*
 insertar(struct Arbol *raizArbol, struct Producto productoInsertar){
@@ -291,5 +355,27 @@ buscar(struct Arbol *raiz, int codigoBuscar){
 	} else{
 
 		return buscar(raiz->hijoDerecho, codigoBuscar);
+	}
+}
+
+int
+modificar(struct Arbol *raiz, int codigoBuscar){
+
+	if (raiz == NULL) {
+
+      return 0;
+	} else if (raiz->productoAlmacenado.codigo == codigoBuscar) {
+
+		printf("%f\n", raiz->productoAlmacenado.precio);
+      float nuevoPrecio;
+      scanf("%f\n", &nuevoPrecio);
+      raiz->productoAlmacenado.precio = nuevoPrecio;
+      return 1;
+	}	else if (codigoBuscar < raiz->productoAlmacenado.codigo) {
+
+		return modificar(raiz->hijoIzquierdo, codigoBuscar);
+	} else{
+
+		return modificar(raiz->hijoDerecho, codigoBuscar);
 	}
 }
