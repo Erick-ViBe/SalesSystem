ccef2986384f94aa93e2f9ac60fd0531c1489f9b
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-22 20:56:37 -0600

    .

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -4,11 +4,11 @@ int main(void) {
 
    struct Arbol *raiz = NULL;
 
+   /*raiz = insertar(raiz, leerProducto());
    raiz = insertar(raiz, leerProducto());
-   raiz = insertar(raiz, leerProducto());
-   raiz = insertar(raiz, leerProducto());
+   raiz = insertar(raiz, leerProducto());*/
 
-   printf("Nombre: %s\n", raiz->productoAlmacenado.nombre);
+   /*printf("Nombre: %s\n", raiz->productoAlmacenado.nombre);
    printf("Codigo: %d\n", raiz->productoAlmacenado.codigo);
    printf("Precio %f\n", raiz->productoAlmacenado.precio);
 
@@ -18,7 +18,66 @@ int main(void) {
 
    printf("Nombre: %s\n", raiz->hijoIzquierdo->productoAlmacenado.nombre);
    printf("Codigo: %d\n", raiz->hijoIzquierdo->productoAlmacenado.codigo);
-   printf("Precio %f\n", raiz->hijoIzquierdo->productoAlmacenado.precio);
+   printf("Precio %f\n", raiz->hijoIzquierdo->productoAlmacenado.precio);*/
+
+
+   int respuesta;
+
+   printf("MENU DE COMPRAS: \n");
+   printf("SELECCIONE LA OPCION: \n\n");
+   printf("OPCION 1: ADMINISTRADOR\n");
+   printf("OPCION 2: CAJERO\n");
+   printf("OPCION 3: SALIR\n");
+
+   scanf("%d", &respuesta);
+   system("clear");
+
+   if(respuesta == 1){
+
+     printf("HA ENTRADO AL MENU ADMINISTRADOR\n");
+     printf("SELECCIONE LA OPCION: \n\n");
+     printf("OPCION 1: INGRESAR PRODUCTOS\n");
+     printf("OPCION 2: MODIFICAR UN PRODUCTO\n");
+     printf("OPCION 3: SALIR\n");
+
+     scanf("%d", &respuesta);
+     system("clear");
+
+     if(respuesta == 1){
+
+       printf("HA SELECCIONADO INGRESAR UN NUEVO PRODUCTO\n");
+
+     } else if(respuesta == 2) {
+
+       printf("HA SELECCIONADO MODIFICAR UN PRODUCTO\n");
+
+     } else {
+
+       printf("LA OPCION INGRESADA NO EXISTE. \n");
+       
+     }
+
+
+   } else if(respuesta == 2) {
+
+     printf("HA ENTRADO AL MENU CAJERO\n");
+     printf("SELECCIONE LA OPCION: \n\n");
+     printf("OPCION 1: REGISTRAR UNA NUEVA COMPRA\n");
+     printf("OPCION 2: SALIR\n");
+
+
+   } else if(respuesta == 3) {
+     printf("GRACIAR POR USAR ESTE PROGRAMA\n");
+     exit(EXIT_SUCCESS);
+
+   } else {
+
+     printf("LA OPCION INGRESADA NO EXISTE. \n");
+     printf("LAS OPCIONES SON: \n\n");
+     printf("OPCION 1: ADMINISTRADOR\n");
+     printf("OPCION 2: CAJERO\n");
+     printf("OPCION 3: SALIR\n");
+   }
 
    return 0;
 }
