02e9ff8796c47e1fd47acd6d8859df10f036258f
Erick-ViBe <erickvb12@gmail.com>
2019-11-23 10:45:28 -0600

    Actualizar

--- Proyecto/supermercado.c
+++ Proyecto/supermercado.c
@@ -219,52 +219,52 @@ modificar(struct Arbol *raiz, int codigoBuscar){
 }
 
 char enteroacaracter(int numero){
-  return numero + '0';
+   return numero + '0';
 }
 
 int obtenerCodigo(char *palabra){
 
-  int acumulado = 0, num = 0;
-  char parte2[8];
+   int acumulado = 0, num = 0;
+   char parte2[8];
 
-  for(int i = 0; i < strlen(palabra); ++i){
+   for(int i = 0; i < strlen(palabra); ++i){
       if(isupper(palabra[i])){
-        palabra[i] = tolower(palabra[i]);
+         palabra[i] = tolower(palabra[i]);
       }
-  }
+   }
 
-  for(int i = 0; i < strlen(palabra); ++i){
-    acumulado += palabra[i];
-  }
+   for(int i = 0; i < strlen(palabra); ++i){
+      acumulado += palabra[i];
+   }
 
-  	char *parte1 = malloc(4 * sizeof(char));
-  	sprintf(parte1, "%d", acumulado);
+   char *parte1 = malloc(4 * sizeof(char));
+   sprintf(parte1, "%d", acumulado);
 
   int valor;
   char convertido;
 
   int i = 0;
-  for(i = 0; i < strlen(palabra) && i < 5; i++){
+   for(i = 0; i < strlen(palabra) && i < 5; i++){
 
-    valor = palabra[i] % 9;
-    if(valor == 0){
-      valor = 9;
-    }
-    convertido = enteroacaracter(valor);
-    parte2[i] = convertido;
-  }
+      valor = palabra[i] % 9;
+      if(valor == 0){
+         valor = 9;
+      }
+      convertido = enteroacaracter(valor);
+      parte2[i] = convertido;
+   }
 
-  parte2[i] = '\0';
+   parte2[i] = '\0';
 
-  strcat(parte1, parte2);
-  num = atoi(parte1);
-  int multiplicador;
+   strcat(parte1, parte2);
+   num = atoi(parte1);
+   int multiplicador;
 
-  if(strlen(parte1) < 10){
-    multiplicador = pow(10, (9 - strlen(parte1)));
-    num *= multiplicador;
-  }
-  return num;
+   if(strlen(parte1) < 10){
+      multiplicador = pow(10,(9-strlen(parte1)));
+      num *= multiplicador;
+   }
+   return num;
 }
 
 void listaInsertar(struct Lista **milista, struct Producto productoAgregado){
