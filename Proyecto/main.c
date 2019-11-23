5765a1935590b131df4b62dd4f58fc2f6a305f66
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-22 23:43:21 -0600

    menu terminado

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -25,7 +25,7 @@ int main(void) {
    char resp;
 
    do {
-
+     system("clear");
      printf("MENU DE COMPRAS: \n");
      printf("SELECCIONE LA OPCION: \n\n");
      printf("OPCION 1: ADMINISTRADOR\n");
@@ -33,71 +33,65 @@ int main(void) {
      printf("OPCION 3: SALIR\n");
 
      scanf("%d", &selector);
+     system("clear");
 
      if(selector == 1){
-
-       printf("HA ENTRADO AL MENU ADMINISTRADOR\n");
+       printf("HA ENTRADO AL MENU ADMINISTRADOR\n\n");
        printf("SELECCIONE LA OPCION: \n\n");
        printf("OPCION 1: INGRESAR PRODUCTOS\n");
        printf("OPCION 2: MODIFICAR UN PRODUCTO\n");
        printf("OPCION 3: SALIR\n");
 
        scanf("%d", &selector);
+       system("clear");
 
        if(selector == 1){
-
          printf("HA SELECCIONADO INGRESAR UN NUEVO PRODUCTO\n");
 
        } else if(selector == 2) {
-
          printf("HA SELECCIONADO MODIFICAR UN PRODUCTO\n");
 
        } else if(selector == 3) {
-
          printf("GRACIAR POR USAR ESTE PROGRAMA\n");
          exit(EXIT_SUCCESS);
 
        } else {
-
          printf("LA OPCION INGRESADA NO EXISTE. \n");
 
        }
 
 
      } else if(selector == 2) {
-
-       printf("HA ENTRADO AL MENU CAJERO\n");
+       printf("HA ENTRADO AL MENU CAJERO\n\n");
        printf("SELECCIONE LA OPCION: \n\n");
        printf("OPCION 1: REGISTRAR UNA NUEVA COMPRA\n");
        printf("OPCION 2: SALIR\n");
 
-       if(selector == 1){
+       scanf("%d", &selector);
+       system("clear");
 
+       if(selector == 1){
          printf("HA SELECCIONADO REGISTRAR UNA NUEVA COMPRA\n");
 
        } else if(selector == 2) {
-
          printf("GRACIAR POR USAR ESTE PROGRAMA\n");
          exit(EXIT_SUCCESS);
 
        } else {
-
          printf("LA OPCION INGRESADA NO EXISTE. \n");
 
        }
 
      } else if(selector == 3) {
-
+       system("clear");
        printf("GRACIAR POR USAR ESTE PROGRAMA\n");
        exit(EXIT_SUCCESS);
 
      } else {
-
        printf("LA OPCION INGRESADA NO EXISTE. \n");
 
      }
-
-     printf("Presione 's' si desea realizar otra operacion\n");
+     printf("\nPresione 's' si desea realizar otra operacion\n");
      scanf("%s", &resp);
 
    } while(resp == 's' || resp == 'S');
